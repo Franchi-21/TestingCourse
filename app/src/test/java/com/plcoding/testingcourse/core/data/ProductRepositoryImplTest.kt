85400690fe686eceb001a287f5bca491483c3632
Francisco <fd687585@gmail.com>
2024-10-26 11:23:16 -0300

    save

---
+++ app/src/test/java/com/plcoding/testingcourse/core/data/ProductRepositoryImplTest.kt
@@ -0,0 +1,47 @@
+package com.plcoding.testingcourse.core.data
+
+import assertk.assertThat
+import assertk.assertions.isTrue
+import com.plcoding.testingcourse.core.domain.AnalyticsLogger
+import com.plcoding.testingcourse.core.domain.LogParam
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import retrofit2.HttpException
+
+class ProductRepositoryImplTest {
+    private lateinit var repository: ProductRepositoryImpl
+    private lateinit var productApi: ProductApi
+    private lateinit var analyticsLogger: AnalyticsLogger
+
+    @BeforeEach
+    fun setUp() {
+        productApi = mockk()
+        analyticsLogger = mockk(relaxed = true)
+        repository = ProductRepositoryImpl(productApi, analyticsLogger)
+    }
+
+    @Test
+    fun `Response error, exception is logged`() = runBlocking {
+        coEvery { productApi.purchaseProducts(any()) } throws mockk<HttpException> {
+            every { code() } returns 404
+            every { message() } returns "Test error"
+        }
+
+        val result = repository.purchaseProducts(listOf())
+
+        assertThat(result.isFailure).isTrue()
+
+        verify {
+            analyticsLogger.logEvent(
+                key = "http_error",
+                LogParam("code", 404),
+                LogParam("message", "Test error")
+            )
+        }
+    }
+}
